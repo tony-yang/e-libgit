c76ce9398dd75a245b6b097df3fb254e837417e3
Tony Yang <tianyuyang658@yahoo.com>
2018-05-27 04:19:54 -0700

    Implement the CLI parsing and connect the CLI with the repo creation through the api

--- ruby_libgit/lib/ruby_libgit/git.rb
+++ ruby_libgit/lib/ruby_libgit/git.rb
@@ -1,3 +1,4 @@
+require 'optparse'
 require 'singleton'
 
 require 'ruby_libgit'
@@ -8,17 +9,72 @@ module RubyLibgit
     include Singleton
 
     def initialize
-      p 'Create a new Git instance'
       RubyLibgit::Logging.logger.info('Create a new Git instance')
-      @init = RubyLibgit::Init.new('testrepo')
+      @options = parse(ARGV)
     end
 
-    def parse
-      p 'Parse argument'
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
+    def parse(args = ARGV)
+      options = {}
+
+      subcommands = {
+        init: OptionParser.new do |o|
+          o.banner = 'usage: git init <repo_name> [<args>] - Create an empty Git repository'
+          options[:init] = {}
+        end
+      }
+
+      parser = OptionParser.new do |o|
+        o.banner = "usage: git <command> [<args>]\n\n"
+        subcommands.each do |subcommand, subparser|
+          o.banner += "Command\n\t" + subcommand.to_s
+          o.banner += "\t\t" + subparser.help
+        end
+
+        o.on_tail('-h', '--help', 'List available subcommands and some concept guides.') do
+          puts o
+          exit
+        end
+      end
+
+      if args[0] && subcommands.key?(args[0].to_sym)
+        subcommand = args.shift.to_sym
+        if args.empty?
+          puts subcommands[subcommand].help
+          exit
+        else
+          begin
+            subcommands[subcommand].order! do |non_option_arg|
+              options[subcommand][:non_option_arg] = non_option_arg
+            end
+          rescue OptionParser::ParseError => error
+            warn error
+            puts subcommands[subcommand].help
+            exit 1
+          end
+        end
+      elsif args.empty?
+        puts parser.help
+        exit
+      else
+        begin
+          parser.parse!(args)
+        rescue OptionParser::ParseError => error
+          warn error
+          puts parser.help
+          exit 1
+        end
+      end
+      options
     end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
 
     def run
-      p 'Git run'
+      if @options.key?(:init)
+        RubyLibgit::Logging.logger.info('Called with init repo_name ' + @options[:init][:non_option_arg])
+        init = RubyLibgit::Init.new(@options[:init][:non_option_arg])
+        init.create_git_repo
+      end
     end
   end
 end
