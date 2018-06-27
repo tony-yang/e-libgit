51f23b2b488712c367b54b0c34e6838a6e83b9db
Tony Yang <tianyuyang658@yahoo.com>
2018-06-26 20:11:06 -0700

    Pygit normalizes the pathname tracked in the .git index file so that we can call pygit add from anywhere within the repo

--- py_libgit/py_libgit/core/index.py
+++ py_libgit/py_libgit/core/index.py
@@ -18,6 +18,7 @@ class Index:
         return tracked_index
 
     def update_index(self, pathname):
+        pathname = self.normalize_pathname(pathname)
         tracked_index = {}
         if os.path.exists(self.index_file):
             tracked_index = self.build_tracked_index()
@@ -29,3 +30,24 @@ class Index:
             return True
 
         return False
+
+    def normalize_pathname(self, pathname):
+        repo_dirs = os.path.dirname(self.git_root).split('/')
+        repo_name = repo_dirs[-1]
+        pathes = pathname.split('/')
+        repo_name_index = 0
+        for i, item in enumerate(pathes):
+            if item == repo_name:
+                repo_name_index = i
+                break
+
+        reduced_pathes = []
+        while i < len(pathes):
+            item = pathes[i]
+            if item and item == '..':
+                reduced_pathes.pop()
+            elif item and item != '.':
+                reduced_pathes.append(item)
+            i += 1
+        normalized_path = '/'.join(reduced_pathes)
+        return normalized_path
--- py_libgit/tests/unit/test_index.py
+++ py_libgit/tests/unit/test_index.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
 
-import os
+import os, shutil
 from py_libgit.core.index import Index
 from py_libgit.core.repo import Repo
 
@@ -9,13 +9,17 @@ import py_libgit.settings_tests
 
 class TestIndex(unittest.TestCase):
     def setUp(self):
-        self.index_file = '/tmp/index'
         repo = MagicMock()
-        repo.get_repo_root = MagicMock(return_value='/tmp')
+        self.repo_name = 'repotest'
+        self.repo_dir = os.path.join('/tmp', self.repo_name)
+        self.git_repo_root = os.path.join(self.repo_dir, '.git')
+        os.makedirs(self.git_repo_root)
+        repo.get_repo_root = MagicMock(return_value=self.git_repo_root)
         self.index = Index(repo)
+        self.index_file = os.path.join(self.repo_dir, '.git', 'index')
 
     def tearDown(self):
-        os.remove(self.index_file)
+        shutil.rmtree(self.repo_dir, ignore_errors=True)
 
     def test_index_updated_correctly_when_no_entry(self):
         pathname = 'helloworld'
@@ -48,3 +52,33 @@ class TestIndex(unittest.TestCase):
 
         expected_index_content = '{}\n{}\n'.format(pathname1, pathname2)
         self.assertEqual(index_content, expected_index_content, 'The index content did not record both pathnames')
+
+    def test_normalize_pathname_removes_single_dot_path(self):
+        pathname = os.path.join(self.repo_dir, 'abc/./test')
+        normalized_pathname = self.index.normalize_pathname(pathname)
+        expected_pathname = os.path.join(self.repo_name, 'abc/test')
+        self.assertEqual(normalized_pathname, expected_pathname, 'The single dot in the normalized path is not properly handled')
+
+    def test_normalize_pathname_removes_double_dots_with_same_path(self):
+        pathname = os.path.join(self.repo_dir, 'abc/test/../test')
+        normalized_pathname = self.index.normalize_pathname(pathname)
+        expected_pathname = os.path.join(self.repo_name, 'abc/test')
+        self.assertEqual(normalized_pathname, expected_pathname, 'The double dot in the normalized path is not properly handled')
+
+    def test_normalize_pathname_removes_double_dots_with_different_path(self):
+        pathname = os.path.join(self.repo_dir, 'abc/test1/../test2')
+        normalized_pathname = self.index.normalize_pathname(pathname)
+        expected_pathname = os.path.join(self.repo_name, 'abc/test2')
+        self.assertEqual(normalized_pathname, expected_pathname, 'The double dot in the normalized path is not properly handled')
+
+    def test_normalize_pathname_removes_multiple_double_dots(self):
+        pathname = os.path.join(self.repo_dir, 'abc/test1/test2/../../test3')
+        normalized_pathname = self.index.normalize_pathname(pathname)
+        expected_pathname = os.path.join(self.repo_name, 'abc/test3')
+        self.assertEqual(normalized_pathname, expected_pathname, 'The double dot in the normalized path is not properly handled')
+
+    def test_normalize_pathname_removes_multiple_double_dots_separately(self):
+        pathname = os.path.join(self.repo_dir, 'abc/test1/../test2/../test3')
+        normalized_pathname = self.index.normalize_pathname(pathname)
+        expected_pathname = os.path.join(self.repo_name, 'abc/test3')
+        self.assertEqual(normalized_pathname, expected_pathname, 'The double dot in the normalized path is not properly handled')
